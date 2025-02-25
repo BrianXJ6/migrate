c0ab4744431df2123fc45237eddbda9a76f17438
Brian Barros <brianferreira15@hotmail.com>
2025-02-25 01:52:35 -0300

    Refactor AppServiceProvider to streamline imports and clean up the register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\{
-    URL,
-    Schema,
-};
-
 use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Sanctum::ignoreMigrations();
+        // ...
     }
 
     /**
